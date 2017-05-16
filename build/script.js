2c00dc129fbdd75190f509304b0e6bf7959c4701
Robert Lowe <RobLowe1992@Yahoo.com>
2017-05-16 09:10:01 -0400

    Added disks to html

--- build/script.js
+++ build/script.js
@@ -1,1 +1,1 @@
-"use strict";$(function(){var t=$(".fly-in-text");setTimeout(function(){t.removeClass("temp-hide")},500)});
+"use strict";$(function(){var t=$(".fly-in-text");setTimeout(function(){t.removeClass("temp-hide")},500);var e=$("canvas");canvas.fill("2d").fillRect(10,10,300,300)});
--- script.js
+++ script.js
@@ -3,4 +3,10 @@ $(function() {
     setTimeout(function() {
         $headerText.removeClass('temp-hide');
     }, 500);
+
+    const $canvas = $('canvas');
+
+    const ctx = canvas.fill('2d');
+
+    ctx.fillRect(10,10,300,300)
 });
